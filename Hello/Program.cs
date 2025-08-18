c5cd7b4e921cf5e294709617d25e75b8ae63ebea
abrar <abrar@DESKTOP-6QVCK9L>
2025-08-18 12:00:01 +0600

    Add project files.

---
+++ Hello/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {/*
+            Console.WriteLine("Hello, World!");
+            Console.Write("asap");
+            Console.WriteLine("Press any key to exit...");
+            Console.Beep();
+            //Console.ReadLine();*/
+
+            /*int x;
+            x = 123;
+            int y = 456;
+            int z = x + y;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);*/
+            /*
+                        //Variables
+                        int age = 28;
+                        Double height = 180.50;
+                        bool alive = true;
+                        Char simble = '@';
+                        string name = "John Doe";
+                        string username = simble + name;
+
+                        Console.WriteLine("Your age is: " + age);
+                        Console.WriteLine("Your height is: " + height + "cm");
+                        Console.WriteLine("Are you alive? " + alive);
+                        Console.WriteLine("Your symbol is: " + simble);
+                        Console.WriteLine("Your name is: " + name);
+                        Console.WriteLine("Your username is: " + username);*/
+            /*
+                        // Constants
+                        const double pi = 3.1416;
+                        Console.WriteLine("The value of pi is: " + pi);*/
+
+
+            //Type Casing
+
+            //User Input
+            /*
+                        Console.WriteLine("What's youe name?");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("What's your age?");
+                        int age = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Hello " + name + ", you are " + age + " years old.");
+
+                        Console.ReadKey();*/
+
+
+            //Arrithmatic Operations
+            int friends = 5;
+            //friends = friends + 1;
+            //friends += 1; // Incrementing friends by 1
+            //friends++; // Incrementing friends by 1
+
+            /*   friends = friends * 2;
+               friends *= 2; // Multiplying friends by 2*/
+
+            // friends = friends / 2;
+            //friends /= 2; // Dividing friends by 2S
+
+            int remainder = friends % 2;
+            Console.WriteLine(remainder);
+
+
+
+
+            //Console.WriteLine(friends);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
