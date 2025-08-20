e854a7be449ebcbbdf709e4b1c0254320888907e
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-08-20 13:45:01 +0600

    Methods

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -609,13 +609,38 @@ namespace Hello
 
             //ForEash Loop
 
-            String[] cars = { "BMW", "Audi", "Tesla", "Toyota", "Tata", };
+            /*String[] cars = { "BMW", "Audi", "Tesla", "Toyota", "Tata", };
 
             foreach (string car in cars)
             {
                 Console.WriteLine(car);
             }
             Console.ReadKey();
+*/
+            /*
+
+
+                        ///Methods
+
+                        // method  = performs a section of code, whenever it's called "invoked".
+                        //           benefit = Let's us reuse code w/o writing it multiple times
+                        //           Good practice is to capitalize method names (I forgot in this video)
+
+                        String name = "Bro";
+                        int age = 21;
+
+                        SingHappyBirthday(name, age);
+
+                        Console.ReadKey();
+                    }
+                    static void SingHappyBirthday(String birthdayBoy, int yearsOld)
+                    {
+                        Console.WriteLine("Happy birthday to you!");
+                        Console.WriteLine("Happy birthday to you!");
+                        Console.WriteLine("Happy birthday dear " + birthdayBoy);
+                        Console.WriteLine("You are " + yearsOld + " years old!");
+                        Console.WriteLine("Happy birthday to you!");
+                        Console.WriteLine();*/
 
 
 
