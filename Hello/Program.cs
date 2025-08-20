23daa558e3ecfbe3cb05909a7b9b798a08c49245
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-08-20 09:58:19 +0600

    Array

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -506,75 +506,92 @@ namespace Hello
 
                         Console.ReadKey();*/
 
-
-            do
-            {
-                double num1 = 0;
-                double num2 = 0;
-                double result = 0;
-
-                Console.WriteLine("........................");
-                Console.WriteLine("Calculator Program");
-                Console.WriteLine("........................");
-
-                Console.Write("Enter number 1: ");
-                num1 = Convert.ToDouble(Console.ReadLine());
-
-                Console.Write("Enter number 2: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter an option: ");
-                Console.WriteLine("\t+ : Add");
-                Console.WriteLine("\t- : Subtract");
-                Console.WriteLine("\t* : Multiply");
-                Console.WriteLine("\t/ : Divide");
-                Console.Write("Enter an option: ");
-
-                switch (Console.ReadLine())
-                {
-                    case "+":
-                        result = num1 + num2;
-                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
-                        break;
-
-                    case "-":
-                        result = num1 - num2;
-                        Console.WriteLine($"Your result: {num1} - {num2} = " + result);
-                        break;
-
-                    case "*":
-                        result = num1 * num2;
-                        Console.WriteLine($"Your result: {num1} * {num2} = " + result);
-                        break;
-
-                    case "/":
-                        if (num2 != 0)
+            /*
+                        do
                         {
-                            result = num1 / num2;
-                            Console.WriteLine($"Your result: {num1} / {num2} = " + result);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Error: Division by zero!");
-                        }
-                        break;
+                            double num1 = 0;
+                            double num2 = 0;
+                            double result = 0;
 
-                    default:
-                        Console.WriteLine("That was not a valid option");
-                        break;
-                }
+                            Console.WriteLine("........................");
+                            Console.WriteLine("Calculator Program");
+                            Console.WriteLine("........................");
 
-                Console.Write("Would you like to continue? (Y = yes, N = No): ");
+                            Console.Write("Enter number 1: ");
+                            num1 = Convert.ToDouble(Console.ReadLine());
 
-            } while (Console.ReadLine().ToUpper() == "Y");
+                            Console.Write("Enter number 2: ");
+                            num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Bye");
+                            Console.WriteLine("Enter an option: ");
+                            Console.WriteLine("\t+ : Add");
+                            Console.WriteLine("\t- : Subtract");
+                            Console.WriteLine("\t* : Multiply");
+                            Console.WriteLine("\t/ : Divide");
+                            Console.Write("Enter an option: ");
+
+                            switch (Console.ReadLine())
+                            {
+                                case "+":
+                                    result = num1 + num2;
+                                    Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                                    break;
+
+                                case "-":
+                                    result = num1 - num2;
+                                    Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                                    break;
+
+                                case "*":
+                                    result = num1 * num2;
+                                    Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                                    break;
+
+                                case "/":
+                                    if (num2 != 0)
+                                    {
+                                        result = num1 / num2;
+                                        Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Error: Division by zero!");
+                                    }
+                                    break;
+
+                                default:
+                                    Console.WriteLine("That was not a valid option");
+                                    break;
+                            }
+
+                            Console.Write("Would you like to continue? (Y = yes, N = No): ");
+
+                        } while (Console.ReadLine().ToUpper() == "Y");
+
+                        Console.WriteLine("Bye");
+                        Console.ReadKey();*/
+
+
+
+
+
+
+
+            //Arrays
+
+
+           String[] cars = { "BMW", "Audi", "Tesla", "Toyota", "Tata", };
+            cars[0] = "Mercedes";
+
+           Console.WriteLine(cars[0]);
+           Console.WriteLine(cars[1]);
+           Console.WriteLine(cars[2]);
+           Console.WriteLine(cars[3]);
+           Console.WriteLine(cars[4]);
+           Console.WriteLine("Total Cars: " + cars.Length);
+
+
             Console.ReadKey();
-
-
-
-
-
 
 
         }
