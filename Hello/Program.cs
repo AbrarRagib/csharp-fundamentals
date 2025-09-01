b4b704290b27dbf3f0a3c77dd212fb7202326fae
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-01 12:27:07 +0600

    arguments

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1297,7 +1297,7 @@ namespace MyFirstProgram
 
 
 
-
+/*
 
 //Abstract Classes
 
@@ -1346,4 +1346,55 @@ namespace MyFirstProgram
 }
 
 
+*/
+
+
+
+
+
+
+
+
+//C# #objects #arguments
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car("Mustang", "red");
+
+            Car car2 = Copy(car1);
+
+            Console.WriteLine(car2.color + " " + car2.model);
+
+            Console.ReadKey();
+        }
+
+        public static void ChangeColor(Car car, String color)
+        {
+            car.color = color;
+        }
+
+        public static Car Copy(Car car)
+        {
+            return new Car(car.model, car.color);
+        }
+    }
+    class Car
+    {
+        public String model;
+        public String color;
+
+        public Car(String model, String color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
 
