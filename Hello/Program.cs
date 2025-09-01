b627544b556553c7982c24936d16c31bfb09a35a
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-01 09:56:29 +0600

    Inheritsance

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1165,10 +1165,10 @@ namespace MyFirstProgram
 */
 
 
+/*
 
 
-
-
+//constractors 
 
 
 
@@ -1220,6 +1220,81 @@ namespace MyFirstProgram
         }
     }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Inheritance
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // inheritance = 1 or more child classes recieving fields, methods, etc. from a common parent
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+
+            Console.WriteLine(bicycle.speed);
+            Console.WriteLine(bicycle.wheels);
+            bicycle.go();
+
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            boat.go();
+
+            Console.ReadKey();
+        }
+    }
+    class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+    }
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+    }
+}
+
+
 
 
 
