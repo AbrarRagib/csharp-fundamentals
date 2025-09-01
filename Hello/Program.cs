f6774a9ae37a8750d327fd9f88ad411618ff5fa7
Abrar Ragib <74444212+AbrarRagib@users.noreply.github.com>
2025-09-01 10:00:28 +0600

    Abstract

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1238,7 +1238,7 @@ namespace MyFirstProgram
 
 
 
-
+/*
 
 //Inheritance
 
@@ -1293,8 +1293,57 @@ namespace MyFirstProgram
         public int wheels = 0;
     }
 }
+*/
 
 
+
+
+
+//Abstract Classes
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // abstract classes =  modifier that indicates missing components or incomplete implementation
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
+            //Vehicle vehicle = new Vehicle(); //can't create a vehicle object
+
+            Console.ReadKey();
+        }
+    }
+    abstract class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("This vehicle is moving!");
+        }
+    }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+        int maxSpeed = 500;
+    }
+    class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+        int maxSpeed = 50;
+    }
+    class Boat : Vehicle
+    {
+        public int wheels = 0;
+        int maxSpeed = 100;
+    }
+}
 
 
 
